8711760a63249b13447d34cdf0a90f63e2efae0a
Bill Mårtensson <bill@magictechnology.se>
2021-11-29 09:30:47 +0100

    Initial Commit

---
+++ piaxiap/piaxiapApp.swift
@@ -0,0 +1,17 @@
+//
+//  piaxiapApp.swift
+//  piaxiap
+//
+//  Created by Bill Martensson on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct piaxiapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
